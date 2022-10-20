210e255cf9a918236b509223450fbbd01383d0fd
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-10-20 03:13:31 +0200

    fragment_login.xml

--- app/src/main/java/com/udacity/shoestore/fragment/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/fragment/LoginFragment.kt
@@ -1,4 +1,4 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
-import com.udacity.shoestore.databinding.FragmentSplashBinding
 
 class LoginFragment : Fragment() {
 
--- app/src/main/java/com/udacity/shoestore/fragment/SplashFragment.kt
+++ app/src/main/java/com/udacity/shoestore/fragment/SplashFragment.kt
@@ -1,4 +1,4 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.fragment
 
 import android.os.Bundle
 import android.os.Handler
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
