ca24702e82af8a36a19c5312fc9930e84b49062e
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-10-20 03:05:01 +0200

    fragment_splash.xml

--- app/src/main/java/com/udacity/shoestore/InsructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/InsructionFragment.kt
@@ -1,28 +1,25 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
-import android.support.v4.media.session.MediaSessionCompat.Token.fromBundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.Person.fromBundle
 import androidx.databinding.DataBindingUtil
-import androidx.media.AudioAttributesCompat.fromBundle
 import androidx.navigation.findNavController
-import com.udacity.shoestore.databinding.FragmentInsructionOneBinding
+import com.udacity.shoestore.databinding.FragmentInsructionBinding
 
-class InsructionOneFragment : Fragment() {
+class InsructionFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentInsructionOneBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_insruction_one, container, false
+        val binding: FragmentInsructionBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_insruction, container, false
         )
         binding.NextButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_insructionOneFragment_to_instrutionTwoFragment2)
+            view.findNavController()
 
         }
         return binding.root
---
+++ app/src/main/java/com/udacity/shoestore/SplashFragment.kt
@@ -0,0 +1,30 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentSplashBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_splash, container, false
+        )
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        },5000)
+
+        return binding.root
+    }
+}
