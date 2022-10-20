f3483439c6e063fbb612f9d6dc26fe621d527224
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-10-20 23:49:16 +0200

    list

---
+++ app/src/main/java/com/udacity/shoestore/fragment/AddItemFragment.kt
@@ -0,0 +1,23 @@
+package com.udacity.shoestore.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentAddItemBinding
+
+class AddItemFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+       val binding :FragmentAddItemBinding = DataBindingUtil.inflate(
+           inflater, R.layout.fragment_add_item,container,false
+       )
+        return binding.root
+    }
+}
